b38b7add9d2d9796f0dcc4684bf362deb4f53e46
aedds <aedds@relias.com>
2020-04-08 11:05:18 -0400

    import common module to be able to use structural directives

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { ValueComponent } from './value/value.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      CommonModule
    ],
    providers: [],
    bootstrap: [
